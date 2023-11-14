8d2ded038195c4e55f9a9387367db0997cf18bd2
majidpawar9 <majidpawar9@gmail.com>
2023-11-14 18:40:52 +0000

    Added and implemented Geolocation function from documentation

--- WeatherWise/ViewModels/WeatherWiseViewModel.cs
+++ WeatherWise/ViewModels/WeatherWiseViewModel.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WeatherWise.ViewModels
 {
-    internal class WeatherWiseViewModel
+    public class WeatherWiseViewModel
     {
+        public ICommand SearchCommand =>
+            new Command(async (searchText) =>
+            {
+                var location = await GetCoordinatesAsync(searchText.ToString());
+            });
+
+        private async Task<Location> GetCoordinatesAsync(string address)
+        {
+            IEnumerable<Location> locations = await Geocoding.Default.GetLocationsAsync(address);
+            Location location = locations?.FirstOrDefault();
+            
+            if(location != null)
+            {
+                Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+            }
+            return location;
+        }
     }
 }
